1f2e5e288025e339097588b93328cedec843faee
Ranga Fernando <rfernando@octet.com>
2018-10-26 17:37:52 +1100

    upload local changes

---
+++ DailyChallenge.Test/Challenge42Test.cs
@@ -0,0 +1,24 @@
+﻿using DailyChallenge.Google;
+using Shouldly;
+using Xunit;
+
+namespace DailyChallenge.test
+{
+    public class Challenge42Test
+    {
+        [Fact]
+        public void ShouldReturnsSubSet()
+        {
+            // arrange
+            var cut = new Challenge42();
+            var list = new int[]{ 12, 1, 61, 5, 9, 2 };
+            var target = 24;
+            var n = new int[] {1, 2};
+            // act
+            var subset = cut.SpitSubset(list, target);
+
+            // assert
+            subset.ShouldBe(n);
+        }
+    }
+}
