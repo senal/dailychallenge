1d8548be53c3ef9fb9592938a1af382c4b76708d
Ranga Fernando <ranga.fernando@gmail.com>
2018-10-29 00:03:13 +1100

    Add one more test to challenge44

--- DailyChallenge.Test/Challenge44Test.cs
+++ DailyChallenge.Test/Challenge44Test.cs
@@ -12,6 +12,7 @@ namespace DailyChallenge.Test
         [InlineData(new int[] { 8, 4, 2, 1 }, new string[] { "(8, 4)", "(8, 2)", "(8, 1)", "(4, 2)", "(4, 1)", "(2, 1)" })]
         [InlineData(new int[] { 2, 4, 1, 3, 5 }, new string[]{ "(2, 1)", "(4, 1)", "(4, 3)" })]
         [InlineData(new int[] { 5, 4, 3, 2, 1 }, new string[] { "(5, 4)", "(5, 3)", "(5, 2)", "(5, 1)", "(4, 3)", "(4, 2)", "(4, 1)", "(3, 2)", "(3, 1)", "(2, 1)" })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new string[] {})]
         public void VerifyInversionPairs(int[] list, string[] expected)
         {
             var cut = new Challenge44();
