48c8cb022ab26d9263edca8966772cdc5b90fc2f
Ranga Fernando <ranga.fernando@gmail.com>
2018-10-28 21:29:13 +1100

    Complete challenge#43 question from Amazon about stack

---
+++ DailyChallenge.Test/Challenge43Test.cs
@@ -0,0 +1,63 @@
+﻿using DailyChallenge.Amazon;
+using Xunit;
+using Shouldly;
+namespace DailyChallenge.Test
+{
+    public class Challenge43Test
+    {
+
+        [Fact]
+        public void ShouldPushMethodIncreaseItemCount()
+        {
+            var cut = new Challenge43();
+
+            cut.Push(1);
+            cut.Push(2);
+            cut.Push(3);
+
+            cut.Count().ShouldBe(3);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3 }, 3, new int[] { 4, 5 }, 5)]
+        [InlineData(new int[] { 20, 30, 45 }, 45, new int[] { 56, 76 }, 76)]
+        public void SholdPopReturnsTopmostElement(int[] initialList, int initialPopValue, int[] secondaryList, int secondaryValue)
+        {
+            var cut = new Challenge43();
+
+            for (int i = 0; i < initialList.Length; i++)
+            {
+                cut.Push(initialList[i]);
+            }
+
+            var item1 = cut.Pop();
+
+            item1.ShouldBe(initialPopValue);
+
+            for (int j = 0; j < secondaryList.Length; j++)
+            {
+                cut.Push(secondaryList[j]);
+            }
+
+            var item2 = cut.Pop();
+
+            item2.ShouldBe(secondaryValue);
+        }
+
+        [Theory]
+        [InlineData(new int[] {1, 2, 3, 4}, 4)]
+        [InlineData(new int[] {23, 56, 98, 102 }, 102)]
+        public void ShouldMaxRetrunsMaximumValueInStack(int[] values, int expectedMaxValue )
+        {
+            var cut = new Challenge43();
+            for (int i = 0; i < values.Length; i++)
+            {
+                cut.Push(values[i]);
+            }
+
+            var max = cut.Max();
+
+            max.ShouldBe(expectedMaxValue);
+        }
+    }
+}
---
+++ DailyChallenge/Amazon/Challenge43.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace DailyChallenge.Amazon
+{
+    /*
+     * 
+     * This problem was asked by Amazon.
+
+        Implement a stack that has the following methods:
+
+        push(val), which pushes an element onto the stack
+        pop(), which pops off and returns the topmost element of the stack. If there are no elements in the stack, then it should throw an error or return null.
+        max(), which returns the maximum value in the stack currently. If there are no elements in the stack, then it should throw an error or return null.
+        Each method should run in constant time.
+     * 
+     */
+    public class Challenge43
+    {
+        private ArrayList list = new ArrayList();
+
+        public void Push(int val)
+        {
+            list.Add(val);
+        }
+
+        public int Pop()
+        {
+            var item = (int)list[list.Count-1];
+            return item;
+        }
+
+        public int Max()
+        {
+            var max = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (max < (int)list[i])
+                    max = (int)list[i];
+            }
+            return max;
+        }
+
+        public int Count() {
+            return list.Count;
+        }
+
+    }
+    
+}
