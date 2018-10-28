6190ad3bf8ff231f92699f97eb074358f1efb9b0
Ranga Fernando <ranga.fernando@gmail.com>
2018-10-28 20:37:12 +1100

    Complete capturing sub arrays which matches to the given value

--- DailyChallenge.Test/Challenge42Test.cs
+++ DailyChallenge.Test/Challenge42Test.cs
@@ -6,19 +6,21 @@ namespace DailyChallenge.test
 {
     public class Challenge42Test
     {
-        [Fact]
-        public void ShouldReturnsSubSet()
+        [Theory]
+        [InlineData(new int[] { 12, 1, 61, 5, 9, 2 }, 24,  new int[] { 12, 9, 2, 1} )]
+        [InlineData(new int[] { 1, 4, 20, 3, 10, 5 }, 33, new int[] { 20, 10, 3 })]
+        [InlineData(new int[] { 1, 4, 0, 0, 3, 10, 5 }, 7, new int[] { 4, 3, 0, 0 })]
+        [InlineData(new int[] { 1, 4 }, 0, new int[] { })]
+        public void ShouldReturnsSubSet(int[] row, int target, int[] expected )
         {
             // arrange
             var cut = new Challenge42();
-            var list = new int[]{ 12, 1, 61, 5, 9, 2 };
-            var target = 24;
-            var n = new int[] {1, 2};
+
             // act
-            var subset = cut.SpitSubset(list, target);
+            var subset = cut.SpitSubset(row, target);
 
             // assert
-            subset.ShouldBe(n);
+            subset.ShouldBe(expected);
         }
     }
 }
--- DailyChallenge/Google/Challenge42.cs
+++ DailyChallenge/Google/Challenge42.cs
@@ -1,19 +1,44 @@
-﻿namespace DailyChallenge.Google
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DailyChallenge.Google
 {
 
     /*
      *
      *  This problem was asked by Google.
-        Given a list of integers S and a target number k, write a function that returns a subset of S that adds up to k. If such a subset cannot be made, then return null.
+        Given a list of integers S and a target number k, write a function that returns a subset of S that adds up to k. 
+        If such a subset cannot be made, then return null.
         Integers can appear more than once in the list. You may assume all numbers in the list are positive.
         For example, given S = [12, 1, 61, 5, 9, 2] and k = 24, return [12, 9, 2, 1] since it sums up to 24.
+        S = [6, 13, 7, 9, 2, 8, 1], K =19
+     *
      */
     public class Challenge42
     {
-
-        public int[] SpitSubset(int[] s, int k)
+        public int[] SpitSubset(int[] s, int expectedSum)
         {
-            return new int[]{};
+            List<int> list = new List<int>();
+            Array.Sort<int>(s, (a, b) => b.CompareTo(a));
+            for (int i = 0; i < s.Length; i++)
+            {
+                var nextIndex = i + 1;
+                var next_element = 0;
+                if (nextIndex < s.Length)
+                    next_element  = s[nextIndex];
+                var current_sum = list.Sum();
+                var current_element = s[i];
+                if (current_element > expectedSum) continue;
+                if (current_sum <= expectedSum) {
+                    if (current_sum + current_element <= expectedSum)
+                    {
+                        if (current_element + next_element <= expectedSum)
+                            list.Add(current_element);
+                    };
+                }
+            }
+            return list.ToArray();
         }
     }
 }
