e3a49606efbe1475765044f95937223dbc8a7387
Ranga Fernando <ranga.fernando@gmail.com>
2018-12-03 00:37:38 +1100

    Leverage bitwise operators and enum/flags types to define a permission model

---
+++ DailyChallenge.Test/PermissionHandlerTest.cs
@@ -0,0 +1,41 @@
+﻿using DailyChallenge.bitwise;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DailyChallenge.Test
+{
+    public class PermissionHandlerTest
+    {
+
+        [Fact]
+        public void VerifyHasExecute() {
+            var permission = 7;
+
+            var p = new PermissionHandler();
+
+            var result = p.CanExecute(permission);
+
+            result.ShouldBe(true);
+        }
+
+
+        [Theory]
+        [InlineData(0, false)]
+        [InlineData(1, true)]
+        [InlineData(2, false)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        [InlineData(7, true)]
+        public void VerifyHasRead(int permission, bool expect)
+        {
+            var p = new PermissionHandler();
+
+            var result = p.CanRead(permission);
+
+            result.ShouldBe(expect);
+        }
+    }
+}
---
+++ DailyChallenge/bitwise/PermissionHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DailyChallenge.bitwise
+{
+    public class PermissionHandler
+    {
+
+        public bool CanExecute(int userPermission) {
+            var got = (Permission)userPermission;
+            return got.HasFlag(Permission.Execute);
+        }
+
+        public bool CanRead(int userPermission) {
+            var got = (Permission)userPermission;
+            return got.HasFlag(Permission.Read);
+        }
+
+        public bool CanWrite(int userPermission) {
+            var got = (Permission)userPermission;
+            return got.HasFlag(Permission.Write);
+        }
+
+    }
+
+    [Flags]
+    public enum Permission
+    {
+        None = 0,
+        Read = 1,
+        Write = 2,
+        Execute = 4,
+
+    }
+
+}
